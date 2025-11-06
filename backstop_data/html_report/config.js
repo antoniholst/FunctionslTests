452ca4dd621695de5d4442d84e8a28cbf1fbc11e
antoniholst <antoninakholostova@gmail.com>
2025-11-06 11:57:28 +0300

    add visual test by backstop

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,78 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Website_Loginpage_0_document_0_browser.png",
+        "test": "../bitmaps_test/20251106-101547/backstop_default_Website_Loginpage_0_document_0_browser.png",
+        "selector": "document",
+        "fileName": "backstop_default_Website_Loginpage_0_document_0_browser.png",
+        "label": "Website Loginpage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.babyblog.ru/login",
+        "referenceUrl": "https://www.babyblog.ru/login",
+        "expect": 0,
+        "viewportLabel": "browser",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Website_Loginpage_0_document_1_phone.png",
+        "test": "../bitmaps_test/20251106-101547/backstop_default_Website_Loginpage_0_document_1_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Website_Loginpage_0_document_1_phone.png",
+        "label": "Website Loginpage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.babyblog.ru/login",
+        "referenceUrl": "https://www.babyblog.ru/login",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Website_Loginpage_0_document_2_tablet.png",
+        "test": "../bitmaps_test/20251106-101547/backstop_default_Website_Loginpage_0_document_2_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Website_Loginpage_0_document_2_tablet.png",
+        "label": "Website Loginpage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.babyblog.ru/login",
+        "referenceUrl": "https://www.babyblog.ru/login",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
